122e7e342f584258b32ac8617d3eb0f4dd3198cb
Oriol Monte <oriol.monte95@gmail.com>
2024-09-25 20:55:56 +0200

    a

---
+++ ExerciciLayouts/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExerciciLayouts
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Calculadora_Click(object sender, RoutedEventArgs e)
+        {
+            Calculadora finestra = new Calculadora();
+            finestra.Show();
+        }
+
+        private void Dibuix_Click(object sender, RoutedEventArgs e)
+        {
+            Dibuix finestra = new Dibuix();
+            finestra.Show();
+        }
+    }
+}
