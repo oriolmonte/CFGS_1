302a82ea1cc74bc7ee9438dbbc314090afe7a5dd
Oriol Monte <oriol.monte95@gmail.com>
2024-10-09 19:41:53 +0200

    dsd

--- ColorPicker/MainWindow.xaml.cs
+++ ColorPicker/MainWindow.xaml.cs
@@ -26,8 +26,51 @@ namespace ColorPicker
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             color = Color.FromArgb((byte)scbA.Value, (byte)scbR.Value, (byte)scbG.Value, (byte)scbB.Value);
-            brush.Color = color;
-            rectangle.Fill = brush;
+            Pinta();
+            colorPicker.SelectedColor= Color.FromArgb((byte)scbA.Value, (byte)scbR.Value, (byte)scbG.Value, (byte)scbB.Value);
         }
+
+        private void scbR_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+                Pinta();
+        }
+
+        private void scbA_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Pinta();
+        }
+        private void Pinta()
+        {
+            if (scbR is not null)
+            {
+                color = Color.FromArgb((byte)scbA.Value, (byte)scbR.Value, (byte)scbG.Value, (byte)scbB.Value);
+                brush.Color = color;
+                rectangle.Fill = brush;
+                colorPicker.SelectedColor=color;
+            }
+
+        }
+
+        private void scbG_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Pinta();
+        }
+
+        private void scbB_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Pinta();
+        }
+
+        private void ColorPicker_SelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
+        {
+            color = colorPicker.SelectedColor.Value;
+            scbA.Value = colorPicker.SelectedColor.Value.A;
+            scbR.Value = colorPicker.SelectedColor.Value.R;
+            scbG.Value = colorPicker.SelectedColor.Value.G;
+            scbB.Value = colorPicker.SelectedColor.Value.B;
+
+            Pinta();
+        }
+
     }
 }
